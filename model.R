9b99e830433893326d6ebecd35beae979955f437
Pariksheet Nanda <pariksheet.nanda@uconn.edu>
2019-08-29 11:20:02 -0400

    Run linear model and add to out plot

--- model.R
+++ model.R
@@ -3,5 +3,11 @@ library(modelr)
 
 sim1
 
+model_lm <- lm(y ~ x, data = sim1)
+coef <- coef(model_lm)
+coef
+
 ggplot(sim1, aes(x, y)) +
-  geom_point()
+  geom_point() +
+  geom_abline(intercept = coef[1],
+              slope = coef[2])
