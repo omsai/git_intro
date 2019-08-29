41f13ae9a4c2f164bd3261cc6fddab4e7d2e6fa7
Pariksheet Nanda <pariksheet.nanda@uconn.edu>
2019-08-29 11:25:13 -0400

    Don't use a linear model

--- model.R
+++ model.R
@@ -3,11 +3,5 @@ library(modelr)
 
 sim1
 
-model_lm <- lm(y ~ x, data = sim1)
-coef <- coef(model_lm)
-coef
-
 ggplot(sim1, aes(x, y)) +
-  geom_point() +
-  geom_abline(intercept = coef[1],
-              slope = coef[2])
+  geom_point()
